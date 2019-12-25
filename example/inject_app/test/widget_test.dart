a02559b52740017479e70364d713952b86852c50
The Viet Nguyen <ntviet18@gmail.com>
2019-12-25 13:33:04 -0600

    Enable Cirrus CI

--- example/inject_app/test/widget_test.dart
+++ example/inject_app/test/widget_test.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:inject_app/app/my_app.dart';
+import 'package:inject_app/inject/inject_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    final container = await InjectApp.create();
+    await tester.pumpWidget(container.getWidget());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
