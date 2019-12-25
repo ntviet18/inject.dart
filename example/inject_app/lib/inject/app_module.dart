4ff7e6058bc4a5816b14e6f76855696881c332e7
The Viet Nguyen <ntviet18@gmail.com>
2019-12-25 12:58:54 -0600

    Apply injection

--- example/inject_app/lib/inject/app_module.dart
+++ example/inject_app/lib/inject/app_module.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/widgets.dart';
 import 'package:inject/inject.dart';
-import 'package:inject_app/main.dart';
+import 'package:inject_app/app/my_app.dart';
 
 @module
 class AppModule {
-
   @provide
   Widget getMaterialApp() => MyApp();
 }
--- example/inject_app/lib/inject/inject_app.dart
+++ example/inject_app/lib/inject/inject_app.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/widgets.dart';
 import 'package:inject/inject.dart';
 import 'package:inject_app/inject/app_module.dart';
+import 'package:inject_app/inject/inject_app.inject.dart' as _g;
 
 @Injector(const [AppModule])
 abstract class InjectApp {
+  static Future<InjectApp> create() =>
+      _g.InjectApp$Injector.create(AppModule());
+
   @provide
   Widget getWidget();
 }
